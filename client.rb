8b76deddad788459b811291e5c97e24e9380fa2a
Andrias Meisyal <andriasonline@gmail.com>
2015-05-28 01:13:10 +0700

    add more image workers

--- client.rb
+++ client.rb
@@ -1,6 +1,5 @@
 require 'base64'
 require 'json'
-require 'net/http'
 require 'rest-client'
 
 def file_name(file)
@@ -19,9 +18,8 @@ def encode(filepath)
   return contents
 end
 
-#worker = Array.new
-
-#worker << Net::HTTP.new('www.example.com', '4567')
+worker = Array.new
+worker << 'http://www.example.com:4567'
 
 # define source and destination path
 imagepath = 'your/image/path/here'
@@ -30,32 +28,42 @@ destinationpath = 'your/destination/image/path/here'
 # add time start
 start = Time.now
 
+# create an item queue
+item_queue = Queue.new
 # read file on directory and push them to queue
-Dir.foreach(imagepath) do |item|
+Dir.foreach(imagepath) do |i|
   # skip reading the parent and current directories
-  next if item == '.' or item == '..'
-  # write log on terminal
-  puts item
-  # encode binary file to base64
-  encoded_data = encode(imagepath + item)
-  # initialize request using POST method
-  request = RestClient.post 'http://www.example.com:4567/convert', :data => {encoded: encoded_data}.to_json, :accept => :json
-  response = JSON.parse(request, :symbolize_names => true)
-  #convert_request = Net::HTTP::Post.new('/convert')
-  #convert_request.body = {encoded: encoded_data}.to_json
-  # do request to server
-  #result = work.request(convert_request)
-  # receive response from server
-  #receive = JSON.parse(result.body, :symbolize_names => true)
-  # write log on terminal if image has been sent
-  puts "#{item} has been sent successfully"
-  # get file name without extensions
-  filename = file_name(item)
-  # get file extensions
-  extensions = file_extensions(item)
-  # decode received data from base64
-  File.open(destinationpath + filename + "_grayscale." + extensions, 'wb') do |f|
-    f.write(Base64.decode64(response[:conversion]))
+  next if i == '.' or i == '..'
+  item_queue.push(i)
+end
+
+worker.each do |work|
+  # loop until there are no more things to do
+  until item_queue.empty?
+    # pop with the non-blocking flag set, this raises
+    # an exception if the queue is empty, in which case
+    # item will be set to nil
+    item = item_queue.pop(true) rescue nil
+    if item
+      # write log on terminal
+      puts item
+      # encode binary file to base64
+      encoded_data = encode(imagepath + item)
+      # initialize request using POST method
+      request = RestClient.post work + '/convert', :data => {encoded: encoded_data}.to_json, :accept => :json
+      # do request to server
+      response = JSON.parse(request, :symbolize_names => true)
+      # write log on terminal if image has been sent
+      puts "#{item} has been sent successfully"
+      # get file name without extensions
+      filename = file_name(item)
+      # get file extensions
+      extensions = file_extensions(item)
+      # decode received data from base64
+      File.open(destinationpath + filename + "_grayscale." + extensions, 'wb') do |f|
+        f.write(Base64.decode64(response[:conversion]))
+      end
+    end
   end
 end
 
